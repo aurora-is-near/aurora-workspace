67127e0e1973d41ebf6d9d7780e7ffb136ec26bf
Evgeny Ukhanov <evgeny@aurora.dev>
2022-03-15 20:37:25 +0100

    Refactore engine-mock, added eth-connecto-mock

--- res/mock_engine/src/fungible_token.rs
+++ res/mock_engine/src/fungible_token.rs
@@ -1,22 +1,39 @@
-use crate::{MockEvmContract, MockEvmContractExt};
+use crate::*;
 use aurora_workspace_types::input::{IsUsedProofCallArgs, StorageBalance};
-use aurora_workspace_types::{output::TransactionStatus, AccountId, Raw};
 use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
-use near_sdk::{borsh, near_bindgen};
+use near_sdk::json_types::U128;
+use near_sdk::serde_json;
+use near_sdk::PromiseOrValue;
 
 #[near_bindgen]
-impl MockEvmContract {
-    pub fn get_version(&self) -> String {
-        "v1".to_string()
+impl MockEngineContract {
+    #[allow(unused_variables)]
+    #[payable]
+    pub fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>) {
+        use std::str::FromStr;
+        assert_eq!(
+            receiver_id,
+            AccountId::from_str("some_account.test").unwrap()
+        );
+        assert_eq!(amount.0, 10);
+        assert_eq!(memo, Some("some message".to_string()));
     }
 
-    #[result_serializer(borsh)]
-    pub fn get_owner(&self) -> AccountId {
-        "owner.test.near".parse().unwrap()
+    #[allow(unused_variables)]
+    pub fn ft_on_transfer(&mut self, sender_id: AccountId, amount: U128, msg: String) -> String {
+        serde_json::to_string(&0).expect("Failed to serialize message")
     }
 
-    pub fn get_chain_id(&self) -> String {
-        1313161556.to_string()
+    #[allow(unused_variables)]
+    #[payable]
+    pub fn ft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<U128> {
+        PromiseOrValue::Value(amount)
     }
 
     pub fn ft_total_eth_supply_on_aurora(&self) -> String {
@@ -42,67 +59,6 @@ impl MockEvmContract {
             reference_hash: None,
             decimals: 18,
         }
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_view(&self, #[serializer(borsh)] _input: Raw) -> TransactionStatus {
-        TransactionStatus::Succeed(vec![])
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_code(&self, #[serializer(borsh)] _input: Raw) -> Raw {
-        // `(string,bool,string) (spiral,true,quasar)`
-        Raw(hex::decode(b"00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000673706972616c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000067175617361720000000000000000000000000000000000000000000000000000").unwrap())
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_storage_at(&self, #[serializer(borsh)] _input: Raw) -> [u8; 32] {
-        [1; 32]
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_erc20_from_nep141(&self, #[serializer(borsh)] _input: Raw) -> AccountId {
-        "erc20.test.near".parse().unwrap()
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_nep141_from_erc20(&self, #[serializer(borsh)] _input: Raw) -> AccountId {
-        "nep141.test.near".parse().unwrap()
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_paused_flags(&self, #[serializer(borsh)] _input: Raw) -> u8 {
-        0
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_bridge_prover(&self) -> AccountId {
-        self.bridge_prover_id.clone()
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_upgrade_index(&self) -> u64 {
-        self.upgrade_delay_blocks
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_paused_precompiles(&self) -> u32 {
-        0u32
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_block_hash(&self, #[serializer(borsh)] _index: u64) -> [u8; 32] {
-        [0u8; 32]
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_balance(&self, #[serializer(borsh)] _address: [u8; 20]) -> [u64; 4] {
-        [0u64; 4]
-    }
-
-    #[result_serializer(borsh)]
-    pub fn get_nonce(&self, #[serializer(borsh)] _address: [u8; 20]) -> [u64; 4] {
-        [0u64; 4]
     }
 
     #[result_serializer(borsh)]
--- res/mock_engine/src/lib.rs
+++ res/mock_engine/src/lib.rs
@@ -1,27 +1,17 @@
-use crate::out::SubmitResult;
+use crate::submit::SubmitResult;
 use aurora_workspace_types::input::{CallInput, DeployErc20Input, NewInput};
-use aurora_workspace_types::output::{Log, TransactionStatus};
-use aurora_workspace_types::{AccountId, Address, Raw, H256};
+use aurora_workspace_types::output::TransactionStatus;
+use aurora_workspace_types::{AccountId, Raw};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen, PanicOnDefault};
 
-pub mod ft;
-mod metadata;
-mod out;
+mod fungible_token;
 mod storage;
-
-fn dummy_submit_result() -> SubmitResult {
-    let log = Log::new(
-        Address::from([1u8; 20]),
-        vec![H256::from([2u8; 32])],
-        vec![3u8; 10],
-    );
-    SubmitResult::new(TransactionStatus::Succeed(vec![0]), 100_000, vec![log])
-}
+mod submit;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct MockEvmContract {
+pub struct MockEngineContract {
     pub chain_id: [u8; 32],
     pub owner_id: AccountId,
     pub bridge_prover_id: AccountId,
@@ -29,10 +19,10 @@ pub struct MockEvmContract {
 }
 
 #[near_bindgen]
-impl MockEvmContract {
+impl MockEngineContract {
     #[init]
-    pub fn new(#[serializer(borsh)] input: NewInput) -> MockEvmContract {
-        MockEvmContract {
+    pub fn new(#[serializer(borsh)] input: NewInput) -> Self {
+        Self {
             chain_id: input.chain_id,
             owner_id: input.owner_id,
             bridge_prover_id: input.bridge_prover_id,
@@ -42,7 +32,7 @@ impl MockEvmContract {
 
     #[result_serializer(borsh)]
     pub fn deploy_code(&mut self, #[serializer(borsh)] _input: Raw) -> SubmitResult {
-        dummy_submit_result()
+        SubmitResult::dummy_submit_result()
     }
 
     #[result_serializer(borsh)]
@@ -52,12 +42,12 @@ impl MockEvmContract {
 
     #[result_serializer(borsh)]
     pub fn call(&mut self, #[serializer(borsh)] _input: CallInput) -> SubmitResult {
-        dummy_submit_result()
+        SubmitResult::dummy_submit_result()
     }
 
     #[result_serializer(borsh)]
     pub fn submit(&mut self, #[serializer(borsh)] _input: Raw) -> SubmitResult {
-        dummy_submit_result()
+        SubmitResult::dummy_submit_result()
     }
 
     #[result_serializer(borsh)]
@@ -85,5 +75,79 @@ impl MockEvmContract {
     #[result_serializer(borsh)]
     pub fn refund_on_error(&mut self, #[serializer(borsh)] _input: Raw) -> u8 {
         0
+    }
+
+    pub fn get_version(&self) -> String {
+        "v1".to_string()
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_owner(&self) -> AccountId {
+        "owner.test.near".parse().unwrap()
+    }
+
+    pub fn get_chain_id(&self) -> String {
+        1313161556.to_string()
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_view(&self, #[serializer(borsh)] _input: Raw) -> TransactionStatus {
+        TransactionStatus::Succeed(vec![])
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_code(&self, #[serializer(borsh)] _input: Raw) -> Raw {
+        // `(string,bool,string) (spiral,true,quasar)`
+        Raw(hex::decode(b"00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000673706972616c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000067175617361720000000000000000000000000000000000000000000000000000").unwrap())
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_storage_at(&self, #[serializer(borsh)] _input: Raw) -> [u8; 32] {
+        [1; 32]
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_erc20_from_nep141(&self, #[serializer(borsh)] _input: Raw) -> AccountId {
+        "erc20.test.near".parse().unwrap()
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_nep141_from_erc20(&self, #[serializer(borsh)] _input: Raw) -> AccountId {
+        "nep141.test.near".parse().unwrap()
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_paused_flags(&self, #[serializer(borsh)] _input: Raw) -> u8 {
+        0
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_bridge_prover(&self) -> AccountId {
+        self.bridge_prover_id.clone()
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_upgrade_index(&self) -> u64 {
+        self.upgrade_delay_blocks
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_paused_precompiles(&self) -> u32 {
+        0u32
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_block_hash(&self, #[serializer(borsh)] _index: u64) -> [u8; 32] {
+        [0u8; 32]
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_balance(&self, #[serializer(borsh)] _address: [u8; 20]) -> [u64; 4] {
+        [0u64; 4]
+    }
+
+    #[result_serializer(borsh)]
+    pub fn get_nonce(&self, #[serializer(borsh)] _address: [u8; 20]) -> [u64; 4] {
+        [0u64; 4]
     }
 }
