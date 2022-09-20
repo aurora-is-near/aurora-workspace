49e20415027477418f96f7b71ef6d307440c49d2
Joshua J. Bouw <joshua@aurora.dev>
2022-09-20 20:01:08 +0400

    feat: export configs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod contract;
 pub mod error;
 pub(crate) mod impls;
 
-pub use contract::EvmContract;
+pub use contract::{EvmContract, ContractConfig, DeployConfig};
 
 pub type Result<T, E = error::Error> = std::result::Result<T, E>;
