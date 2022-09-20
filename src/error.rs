854d3385fc47aee9b91ad42c56d178c853bf802f
Joshua J. Bouw <joshua@aurora.dev>
2022-09-20 19:59:58 +0400

    feat: replace test specifics and improve API

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,17 @@
-use thiserror::Error;
+use std::io;
 
-#[derive(Error, Debug)]
-pub enum DataError {
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
     #[error(transparent)]
     FromUtf8Error(#[from] std::string::FromUtf8Error),
-    #[error("invalid address")]
-    AddressError(#[from] std::),
-}
-
-pub enum EvmContractError {
-
+    #[error(transparent)]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error(transparent)]
+    ParseAccountError(#[from] near_account_id::ParseAccountError),
+    #[error(transparent)]
+    Workspace(#[from] workspaces::error::Error),
+    #[error(transparent)]
+    WorkspaceExecutionFailure(#[from] workspaces::result::ExecutionFailure),
+    #[error(transparent)]
+    Io(#[from] io::Error),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 mod contract;
 pub mod error;
+pub(crate) mod impls;
 
 pub use contract::EvmContract;
+
+pub type Result<T, E = error::Error> = std::result::Result<T, E>;
