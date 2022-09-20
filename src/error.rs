b7dfb0a82a1c6ce7841fdfe4b635ec86b86d0393
Joshua J. Bouw <joshua@aurora.dev>
2022-09-20 20:11:10 +0400

    feat: box errors

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,8 @@
 use std::io;
 
+#[allow(clippy::large_enum_variant)]
 #[derive(Debug, thiserror::Error)]
-pub enum Error {
+pub enum ErrorKind {
     #[error(transparent)]
     FromUtf8Error(#[from] std::string::FromUtf8Error),
     #[error(transparent)]
@@ -14,4 +15,14 @@ pub enum Error {
     WorkspaceExecutionFailure(#[from] workspaces::result::ExecutionFailure),
     #[error(transparent)]
     Io(#[from] io::Error),
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+pub struct Error(Box<ErrorKind>);
+
+impl<E> From<E> for Error where ErrorKind: From<E> {
+    fn from(err: E) -> Self {
+        Error(Box::new(ErrorKind::from(err)))
+    }
 }
