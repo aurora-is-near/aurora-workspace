a3bef40758f7f36508350cac9fe551a7f40ab57f
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-03-06 11:55:39 +0100

    feat: add initial github ci support

--- res/mock_evm/build.sh
+++ res/mock_evm/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-set -e && RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release && mkdir -p ../bin && cp target/wasm32-unknown-unknown/release/*.wasm ../bin/main.wasm
+set -e
+RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release && mkdir -p ../bin && cp target/wasm32-unknown-unknown/release/*.wasm ../bin/main.wasm
