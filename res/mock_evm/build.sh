b7207c13d1f8df70e9ef646fea643445a4e46ce8
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-03-06 12:06:40 +0100

    chore: fix github ci

--- res/mock_evm/build.sh
+++ res/mock_evm/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
+rustup toolchain install nightly -t wasm32-unknown-unknown
 RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release && mkdir -p ../bin && cp target/wasm32-unknown-unknown/release/*.wasm ../bin/main.wasm
