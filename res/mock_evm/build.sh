6724a562938ed2819aef4a6949bb6567249926a4
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-03-06 12:13:24 +0100

    chore: fix target #2

--- res/mock_evm/build.sh
+++ res/mock_evm/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-rustup target add wasm32-unknown-unknown
-RUSTFLAGS='-C link-arg=-s' cargo +nightly build --target wasm32-unknown-unknown --release && mkdir -p ../bin && cp target/wasm32-unknown-unknown/release/*.wasm ../bin/main.wasm
+RUSTFLAGS='-C link-arg=-s' cargo build --release && mkdir -p ../bin && cp target/wasm32-unknown-unknown/release/*.wasm ../bin/main.wasm
